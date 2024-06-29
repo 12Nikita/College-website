6f266c2d7b022a5959143582c1b57f0babaa4812
NIKITA <116105818+12Nikita@users.noreply.github.com>
2024-06-29 22:18:37 +0530

    Delete Assignment 2 directory

--- Assignment 2/12.js
+++
@@ -1,8 +0,0 @@
-//Q12.Validate whether a given value is a RegExp or not:
-
-function isRegExp(value) {
-  return value instanceof RegExp;
-}
-
-console.log(isRegExp(/test/)); // Output: true
-console.log(isRegExp('hello')); // Output: false
--- Assignment 2/8.js
+++
@@ -1,8 +0,0 @@
-//Q8.Sort the characters in a string alphabetically:
-
-function sortStringAlphabetically(str) {
-  return str.split('').sort().join('');
-}
-
-var string = "hello world";
-console.log(sortStringAlphabetically(string)); // Output: " dehllloorw"
